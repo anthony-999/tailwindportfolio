f80f9a32981ad5f27a9d9c28531afbe7c4b183e0
Anthony Valdez <anthonyvaldez99999@gmail.com>
2023-03-19 20:35:45 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {
+        fontFamily: {
+          'Poppins': ['Poppins'],
+       },
+      
+       letterSpacing: {
+        tightest: '-.075em',
+        tighter: '-.05em',
+        tight: '-.025em',
+        normal: '0',
+        wide: '.025em',
+        wider: '.05em',
+        widest: '.1em',
+        widest: '.25em',
+      }
+      },
+    },
+    plugins: [],
+  }
